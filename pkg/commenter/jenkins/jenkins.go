f81d2772e3a6e3ef05d69bd4933adb17f91facdd
MorAlon1 <mor.alon@aquasec.com>
2022-09-04 15:26:01 +0300

    check clone url in jenkins

--- pkg/commenter/jenkins/jenkins.go
+++ pkg/commenter/jenkins/jenkins.go
@@ -2,7 +2,11 @@ package jenkins
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/aquasecurity/go-git-pr-commenter/pkg/commenter"
 	"github.com/aquasecurity/go-git-pr-commenter/pkg/commenter/bitbucket"
@@ -21,8 +25,60 @@ type bitbucketPayload struct {
 	Repository bitbucketRepositoryPayload
 }
 
+func getRepositoryCloneURL(repositoryPath string) (string, error) {
+	if cloneUrl, isExist := os.LookupEnv("GIT_URL"); isExist {
+		return cloneUrl, nil
+	}
+	return GetGitRemoteURL(repositoryPath)
+}
+
+func GetGitRemoteURL(repositoryPath string) (string, error) {
+	remotes, err := getGitRemotes(repositoryPath)
+	if err != nil {
+		return "", err
+	}
+
+	if len(remotes) == 0 {
+		return "", errors.New("No git remotes found")
+	}
+
+	for _, remote := range remotes {
+		if remote[0] == "origin" {
+			return remote[1], nil
+		}
+	}
+
+	return remotes[0][1], nil
+}
+
+func getGitRemotes(repositoryPath string) ([][]string, error) {
+	gitPath, err := exec.LookPath("git")
+	if err != nil {
+		return nil, errors.New("git not found")
+	}
+
+	cmd := exec.Command(gitPath, "remote", "-v")
+	cmd.Dir = repositoryPath
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, fmt.Errorf(`failed to execute git command "%s" - %s - %s`, cmd.String(), output, err.Error())
+	}
+
+	outputAsString := string(output)
+	outputAsString = strings.TrimSuffix(outputAsString, "\n")
+	lines := strings.Split(outputAsString, "\n")
+	remotes := [][]string{}
+	for _, line := range lines {
+		remotes = append(remotes, strings.Fields(line))
+	}
+	return remotes, nil
+}
+
 func NewJenkins() (commenter.Repository, error) {
-	if _, exist := os.LookupEnv("BITBUCKET_ACTOR"); exist {
+	repositoryPath := os.Getenv("WORKSPACE")
+	cloneUrl, _ := getRepositoryCloneURL(repositoryPath)
+
+	if strings.Contains(cloneUrl, "bitbucket.org") {
 		payload := &bitbucketPayload{}
 		err := json.Unmarshal([]byte(os.Getenv("BITBUCKET_PAYLOAD")), payload)
 		if err != nil {
